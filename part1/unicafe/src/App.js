db7c041b9e33cde0f7c7e36adbd549a052929463
nalinsinha <nlnsnh@gmail.com>
2022-05-14 18:04:03 -0400

    Exercise 1.7 unicafe p2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,16 +17,35 @@ const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+	const [sum, setSum] = useState(0);
+	const [total, setTotal] = useState(0);
+	const handleGood = () => {
+		setGood(good + 1)
+		setTotal(total + 1)
+		setSum(sum + 1)
+	}
+	const handleNeutral = () => {
+		setNeutral(neutral + 1)
+		setTotal(total + 1)
+	}
+	const handleBad = () => {
+		setBad(bad + 1)
+		setTotal(total + 1)
+		setSum(sum - 1)
+	}
 
 	return <div>
 		<Header text='give feedback'/>
-		<Button text='good' handleClick={() => setGood(good + 1)}/>
-		<Button text='neutral'handleClick={() => setNeutral(neutral + 1)}/>
-		<Button text='bad' handleClick={() => setBad(bad + 1)}/>
+		<Button text='good' handleClick={handleGood}/>
+		<Button text='neutral'handleClick={handleNeutral}/>
+		<Button text='bad' handleClick={handleBad}/>
 		<Header text='statistics'/>
 		<p>Good: {good}</p>
 		<p>Neutral: {neutral}</p>
 		<p>Bad: {bad}</p>
+		<p>All: {sum}</p>
+		<p>Average: {sum / total}</p>
+		<p>positive: {(good/total) * 100}%</p>
 	</div>;
 };
 
