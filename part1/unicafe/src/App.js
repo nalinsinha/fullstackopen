56889eeaee6575d630b5a6de905ccd5460276a0b
nalinsinha <nlnsnh@gmail.com>
2022-05-14 18:10:23 -0400

    Exercise 1.8 unicafe p3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
 
-const Header = ({text}) => {
+const Header = ({ text }) => {
 	return <h1>{text}</h1>;
 };
 
-const Button = ({text, handleClick}) => {
+const Button = ({ text, handleClick }) => {
+	return <button onClick={handleClick}>{text}</button>;
+};
+
+const Statistics = ({good, neutral, bad, sum, total}) => {
 	return (
-		<button onClick={handleClick}>
-			{text}
-		</button>
-	)
-}
+		<>
+			<Header text="statistics" />
+			<p>Good: {good}</p>
+			<p>Neutral: {neutral}</p>
+			<p>Bad: {bad}</p>
+			<p>All: {sum}</p>
+			<p>Average: {sum / total}</p>
+			<p>positive: {(good / total) * 100}%</p>
+		</>
+	);
+};
 
 const App = () => {
 	// save clicks of each button to its own state
@@ -20,33 +30,29 @@ const App = () => {
 	const [sum, setSum] = useState(0);
 	const [total, setTotal] = useState(0);
 	const handleGood = () => {
-		setGood(good + 1)
-		setTotal(total + 1)
-		setSum(sum + 1)
-	}
+		setGood(good + 1);
+		setTotal(total + 1);
+		setSum(sum + 1);
+	};
 	const handleNeutral = () => {
-		setNeutral(neutral + 1)
-		setTotal(total + 1)
-	}
+		setNeutral(neutral + 1);
+		setTotal(total + 1);
+	};
 	const handleBad = () => {
-		setBad(bad + 1)
-		setTotal(total + 1)
-		setSum(sum - 1)
-	}
+		setBad(bad + 1);
+		setTotal(total + 1);
+		setSum(sum - 1);
+	};
 
-	return <div>
-		<Header text='give feedback'/>
-		<Button text='good' handleClick={handleGood}/>
-		<Button text='neutral'handleClick={handleNeutral}/>
-		<Button text='bad' handleClick={handleBad}/>
-		<Header text='statistics'/>
-		<p>Good: {good}</p>
-		<p>Neutral: {neutral}</p>
-		<p>Bad: {bad}</p>
-		<p>All: {sum}</p>
-		<p>Average: {sum / total}</p>
-		<p>positive: {(good/total) * 100}%</p>
-	</div>;
+	return (
+		<div>
+			<Header text="give feedback" />
+			<Button text="good" handleClick={handleGood} />
+			<Button text="neutral" handleClick={handleNeutral} />
+			<Button text="bad" handleClick={handleBad} />
+			<Statistics good={good} bad={bad} neutral={neutral} sum={sum} total={total}/>
+		</div>
+	);
 };
 
 export default App;
