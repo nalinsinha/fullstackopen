c36ede06db8de1ecd6860c4a6c07bbde687ed395
nalinsinha <nlnsnh@gmail.com>
2022-05-25 20:27:49 -0400

    Exercise 2.16 phonebook p8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,101 +1,130 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import entry from "./services/entry";
 
-const Filter = ({search, handleSearchChange}) => {
+const Filter = ({ search, handleSearchChange }) => {
 	return (
 		<form>
 			<div>
-				filter shown with <input value={search} onChange={handleSearchChange}/>
+				filter shown with{" "}
+				<input value={search} onChange={handleSearchChange} />
 			</div>
 		</form>
-	)
-}
+	);
+};
 
-const PersonForm = ({addName, newName, handleNameChange, newPhone, handlePhoneChange}) => {
-	return(
+const PersonForm = ({
+	addName,
+	newName,
+	handleNameChange,
+	newPhone,
+	handlePhoneChange,
+}) => {
+	return (
 		<form onSubmit={addName}>
 			<div>
-					name: <input value={newName} onChange={handleNameChange} />
-				</div>
-				<div>
-					number: <input value={newPhone} onChange={handlePhoneChange} />
-				</div>
-				<div>
-					<button type="submit" >add</button>
+				name: <input value={newName} onChange={handleNameChange} />
+			</div>
+			<div>
+				number: <input value={newPhone} onChange={handlePhoneChange} />
+			</div>
+			<div>
+				<button type="submit">add</button>
 			</div>
 		</form>
-	)
-}
+	);
+};
 
-const Persons = ({ personsToShow }) => { 
-	console.log("personstoShow: ", personsToShow)
+const Persons = ({ personsToShow }) => {
+	console.log("personstoShow: ", personsToShow);
 	return (
 		<>
-			{
-				personsToShow.map(person => <Person key={person.name} person={person}/>)
-			}
+			{personsToShow.map((person) => (
+				<Person key={person.name} person={person} />
+			))}
 		</>
-	)
-}
+	);
+};
 
-const Person = ({person}) => {
+const Person = ({ person }) => {
 	return (
-		<p key={person.name}>{person.name} {person.phone}</p>
-	)
-}
+		<p key={person.name}>
+			{person.name} {person.number}
+		</p>
+	);
+};
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: "040-1234567" }]);
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", number: "040-1234567" },
+	]);
 	const [newName, setNewName] = useState("");
 	const [newPhone, setNewPhone] = useState("");
 	const [showAll, setShowAll] = useState(true);
-	const [search, setSearch] = useState("")
+	const [search, setSearch] = useState("");
 
-	
-	const personsToShow = showAll ? persons : persons.filter(person => person.name.toLowerCase().startsWith(search.toLowerCase(), 0))
+	useEffect(() => {
+		console.log("effect");
+		entry.getAll().then(response => {
+			setPersons(response.data)
+		})
+	}, []);
+
+	const personsToShow = showAll
+		? persons
+		: persons.filter((person) =>
+				person.name.toLowerCase().startsWith(search.toLowerCase(), 0)
+		  );
 
 	const handleNameChange = (event) => {
-		setNewName(event.target.value)
+		setNewName(event.target.value);
 	};
 	const handlePhoneChange = (event) => {
-		setNewPhone(event.target.value)
+		setNewPhone(event.target.value);
 	};
 	const handleSearchChange = (event) => {
-		setSearch(event.target.value)
-		if(search.length === 0) {
-			setShowAll(true)
+		setSearch(event.target.value);
+		if (event.target.value.length === 0) {
+			setShowAll(true);
+		} else {
+			setShowAll(false);
 		}
-		else {
-			setShowAll(false)
-		}
-	}
-	const message = `${newName} is already added to phonebook`
+	};
+	const message = `${newName} is already added to phonebook`;
 
 	const addName = (event) => {
-		event.preventDefault()
-		if(persons.find(person => person.name === newName)) {
-			alert(message)
-		}
-		else {
+		event.preventDefault();
+		if (persons.find((person) => person.name === newName)) {
+			alert(message);
+		} else {
 			const temp = {
 				name: newName,
-				phone: newPhone,
-			}
-			setPersons(persons.concat(temp))
+				number: newPhone,
+				id: (persons.length + 1),
+			};
+			entry.create(temp).then(response => {
+				setPersons(persons.concat(response.data));
+			})
+			
 		}
-		setNewName("")
-		setNewPhone("")
-		
-	}
-
+		setNewName("");
+		setNewPhone("");
+	};
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<Filter search={search} handleSearchChange={handleSearchChange}/>
+			<Filter search={search} handleSearchChange={handleSearchChange} />
 			<h3>Add a new</h3>
-			<PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newPhone={newPhone} handlePhoneChange={handlePhoneChange}/>
+			<PersonForm
+				addName={addName}
+				newName={newName}
+				handleNameChange={handleNameChange}
+				newPhone={newPhone}
+				handlePhoneChange={handlePhoneChange}
+			/>
 			<h3>Numbers</h3>
-			<Persons personsToShow={personsToShow}/>
+			<Persons personsToShow={personsToShow} />
 		</div>
 	);
 };
